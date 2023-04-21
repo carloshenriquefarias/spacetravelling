7ab4e69cef3cebb9d5c9434b92ec5b0b9d92f265
Prisco Cleyton <you@example.comcarloshenriquepvh@hotmail.com>
2023-04-20 20:24:14 -0400

    aprimorando e removendo itens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,9 @@ import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 import Header from '../components/Header';
 
+import Select from 'react-select'
+import makeAnimated from "react-select/animated";
+
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -32,59 +35,94 @@ interface HomeProps {
   preview: boolean;
 }
 
+const options = [
+  { value: "produto 01", label: "Produto 01" },
+  { value: "produto 02", label: "Produto 02" },
+  { value: "produto 03", label: "Produto 03" },
+  { value: "produto 04", label: "Produto 04" },
+  { value: "produto 05", label: "Produto 05" },
+  { value: "produto 06", label: "Produto 06" },
+  { value: "produto 07", label: "Produto 07" },
+  { value: "produto 08", label: "Produto 08" },
+];
+
 export default function Home({ postsPagination, preview }: HomeProps) {
-  const formattedPost = postsPagination.results.map(post => {
-    return {
-      ...post,
-      first_publication_date: format(
-        new Date(post.first_publication_date),
-        'dd MMM yyyy',
-        {
-          locale: ptBR,
-        }
-      ),
-    };
-  });
 
-  const [posts, setPosts] = useState<Post[]>(formattedPost);
-  const [nextPage, setNextPage] = useState(postsPagination.next_page);
-  const [currentPage, setCurrentPage] = useState(1);
+  const animatedComponents = makeAnimated();
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const handleSelect = () => {
+      console.log(selectedOptions);
+  };
 
-  async function handleNextPage(): Promise<void> {
-    if (currentPage !== 1 && nextPage === null) {
-      return;
-    }
 
-    const postsResults = await fetch(`${nextPage}`).then(response =>
-      response.json()
-    );
-    setNextPage(postsResults.next_page);
-    setCurrentPage(postsResults.page);
+  // const formattedPost = postsPagination.results.map(post => {
+  //   return {
+  //     ...post,
+  //     first_publication_date: format(
+  //       new Date(post.first_publication_date),
+  //       'dd MMM yyyy',
+  //       {
+  //         locale: ptBR,
+  //       }
+  //     ),
+  //   };
+  // });
 
-    const newPosts = postsResults.results.map(post => {
-      return {
-        uid: post.uid,
-        first_publication_date: format(
-          new Date(post.first_publication_date),
-          'dd MMM yyyy',
-          {
-            locale: ptBR,
-          }
-        ),
-        data: {
-          title: post.data.title,
-          subtitle: post.data.subtitle,
-          author: post.data.author,
-        },
-      };
-    });
+  // const [posts, setPosts] = useState<Post[]>(formattedPost);
+  // const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  // const [currentPage, setCurrentPage] = useState(1);
 
-    setPosts([...posts, ...newPosts]);
-  }
+  // async function handleNextPage(): Promise<void> {
+  //   if (currentPage !== 1 && nextPage === null) {
+  //     return;
+  //   }
+
+  //   const postsResults = await fetch(`${nextPage}`).then(response =>
+  //     response.json()
+  //   );
+  //   setNextPage(postsResults.next_page);
+  //   setCurrentPage(postsResults.page);
+
+  //   const newPosts = postsResults.results.map(post => {
+  //     return {
+  //       uid: post.uid,
+  //       first_publication_date: format(
+  //         new Date(post.first_publication_date),
+  //         'dd MMM yyyy',
+  //         {
+  //           locale: ptBR,
+  //         }
+  //       ),
+  //       data: {
+  //         title: post.data.title,
+  //         subtitle: post.data.subtitle,
+  //         author: post.data.author,
+  //       },
+  //     };
+  //   });
+
+  //   setPosts([...posts, ...newPosts]);
+  // }
 
   return (
     <>
-      <main className={commonStyles.container}>
+      <h1>Ola mundo</h1>
+
+      <Select
+          defaultValue={[options[0], options[2]]}
+          components={animatedComponents}
+          isMulti
+          options={options}
+          // onChange={(item) => setSelectedOptions(item)}
+          className="select"
+          isClearable={true}
+          isSearchable={true}
+          isDisabled={false}
+          isLoading={false}
+          isRtl={false}
+          closeMenuOnSelect={false}
+      />
+      {/* <main className={commonStyles.container}>
         <Header />
 
         <div className={styles.posts}>
@@ -120,42 +158,42 @@ export default function Home({ postsPagination, preview }: HomeProps) {
             </Link>
           </aside>
         )}
-      </main>
+      </main> */}
     </>
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
-  const prismic = getPrismicClient();
-  const postsResponse = await prismic.query(
-    [Prismic.Predicates.at('document.type', 'posts')],
-    {
-      pageSize: 3,
-      orderings: '[document.last_publication_date desc]',
-    }
-  );
+// export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
+//   const prismic = getPrismicClient();
+//   const postsResponse = await prismic.query(
+//     [Prismic.Predicates.at('document.type', 'posts')],
+//     {
+//       pageSize: 3,
+//       orderings: '[document.last_publication_date desc]',
+//     }
+//   );
 
-  const posts = postsResponse.results.map(post => {
-    return {
-      uid: post.uid,
-      first_publication_date: post.first_publication_date,
-      data: {
-        title: post.data.title,
-        subtitle: post.data.subtitle,
-        author: post.data.author,
-      },
-    };
-  });
+//   const posts = postsResponse.results.map(post => {
+//     return {
+//       uid: post.uid,
+//       first_publication_date: post.first_publication_date,
+//       data: {
+//         title: post.data.title,
+//         subtitle: post.data.subtitle,
+//         author: post.data.author,
+//       },
+//     };
+//   });
 
-  const postsPagination = {
-    next_page: postsResponse.next_page,
-    results: posts,
-  };
-  return {
-    props: {
-      postsPagination,
-      preview,
-    },
-    revalidate: 1800,
-  };
-};
+//   const postsPagination = {
+//     next_page: postsResponse.next_page,
+//     results: posts,
+//   };
+//   return {
+//     props: {
+//       postsPagination,
+//       preview,
+//     },
+//     revalidate: 1800,
+//   };
+// };
